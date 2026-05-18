7f2ab4dbad5a44a985051bd6a4faa6fb1f375169
bordoste2-cmyk <bordo.ste2@gmail.com>
2026-05-18 14:54:32 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Diario Alimentare - Service Worker
 // Update the version number when you change app code to force refresh
-const CACHE_NAME = 'diario-v6';
+const CACHE_NAME = 'diario-v7';
 const ASSETS = [
   './',
   './index.html',
