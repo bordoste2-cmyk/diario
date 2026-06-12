71d4607b075d22baa69717a2d6f8b10369969da2
bordoste2-cmyk <bordo.ste2@gmail.com>
2026-06-12 19:05:31 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Health Diary - Service Worker
 // Update the version number when you change app code to force refresh
-const CACHE_NAME = 'diario-v3.15.2';
+const CACHE_NAME = 'diario-v3.16.0';
 const ASSETS = [
   './',
   './index.html',
